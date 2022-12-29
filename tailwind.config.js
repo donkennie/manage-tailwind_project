d60224b734017ead821c184df52822d31077c379
donkennie <ajeigbekehinde160@gmail.com>
2022-12-29 21:44:40 +0100

    config tailwind screen size and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+      }
+    },
   },
   plugins: [],
 }
